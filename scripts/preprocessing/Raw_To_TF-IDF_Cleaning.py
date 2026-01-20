6ed15eb8a2609d6e4513d6c94a28fb17c327ba1f
HarveyAppleyard <h.j.appleyard2@newcastle.ac.uk>
2026-01-20 21:09:35 +0000

    Neater, shorter code

--- scripts/preprocessing/Raw_To_TF-IDF_Cleaning.py
+++ scripts/preprocessing/Raw_To_TF-IDF_Cleaning.py
@@ -9,14 +9,10 @@ cleaned = df[['id', 'title', 'genres', 'overview']].copy()
 #Drop row if no id, title or overview
 cleaned.dropna(subset=['id', 'title', 'overview'], inplace=True)
 
-#Ensure text columns are strings (prevents float / NaN issues)
-for column in ['title', 'genres', 'overview']:
+#Ensure columns are strings (prevents float / NaN issues) and strip
+for column in cleaned.columns:
     cleaned[column] = cleaned[column].astype(str)
-
-#Strip leading and trailing whitespace
-cleaned['title'] = cleaned['title'].str.strip()
-cleaned['genres'] = cleaned['genres'].str.strip()
-cleaned['overview'] = cleaned['overview'].str.strip()
+    cleaned[column] = cleaned[column].str.strip()
 
 #drop rows with duplicated ids
 cleaned.drop_duplicates(subset=['id'], inplace=True)
